9f32ab608a6fd0d9aaf924b9f8ce1b16fecae803
Tatiana Quintana <tatsqui@gmail.com>
2019-04-08 15:14:08 -0700

    adding show method

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,15 +1,22 @@
 TASKS = [
-  "Set alarm", 
-  "Make Breakfast", 
-  "Make Lunch", 
-  "Make Dinner", 
-  "Exercise", 
-  "Do Homework",
+  {id: 1, task: "Set alarm"}, 
+  {id: 2, task: "Make Breakfast"}, 
+  {id: 3, task: "Make Lunch"}, 
+  {id: 4, task: "Make Dinner"}, 
+  {id: 5, task: "Exercise"}, 
+  {id: 6, task: "Do Homework"}
 ]
 
 class TasksController < ApplicationController
   def index
     # add an instance variable here
     @tasks = TASKS
+  end
+
+  def show
+    task_id = params[:id]
+
+    @task = TASKS[task_id]
+   # @task = Task.find(task_id) -- when we learn about Models
   end
 end
