4a5e6276005a895b7d90df886284cb3a96c90f3e
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-04-08 14:49:19 -0700

    added Tasks Controller and its index action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,11 @@
 class TasksController < ApplicationController
+  TASKS = [
+    {name: "Practice CSS"},
+    {name: "Celebrate Wedding Anniversary"},
+    {name: "Stress Eat!"},
+  ]
+
   def index
+    @tasks = TASKS
   end
 end
