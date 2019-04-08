f96a180d39e0667ae111bf4014d03e778c9a04a9
Alexandria McCarthy <alexandria.mccarthy77@gmail.com>
2019-04-08 14:57:44 -0700

    created Tasks controller, route to view task index page, and ERB view to display tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,7 @@
 class TasksController < ApplicationController
+  TASKS = [{task: "water pothos", status: "complete"}, {task: "make dinner", status: "incomplete"}, {task: "finish hw", status: "incomplete"}]
+
+  def index
+    @tasks = TASKS
+  end
 end
