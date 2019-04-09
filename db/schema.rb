6ebc7e63583c9ff225910b3d05f37d0c4e1c0475
Tatiana Quintana <tatsqui@gmail.com>
2019-04-09 16:47:34 -0700

    changed completion_date to date data type

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_09_223759) do
+ActiveRecord::Schema.define(version: 2019_04_09_233419) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2019_04_09_223759) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.string "description"
-    t.string "date"
+    t.date "completion_date"
   end
 
 end
