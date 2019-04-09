ee657203d758b4dc8f28011a4ea91109a0e163b2
qqdipps <sdipp007@gmail.com>
2019-04-09 16:13:42 -0700

    removed file, not required

--- config/initializers/active_record.rb
+++
@@ -1,4 +0,0 @@
-ActiveRecord::ConnectionAdapters::PostgreSQLAdapter::
-  NATIVE_DATABASE_TYPES[:datetime] = {name: "timestamp with time zone"} 
-
-  #  Rails doesn’t support time with time zone, code above added so it should work. =)
--- config/initializers/time_zone.rb
+++ config/initializers/time_zone.rb
@@ -1,1 +1,3 @@
 Time.zone = "America/Los_Angeles"
+
+# Use .current to get current time rather than .now which will use
