b33e45a7b0eaefe63cff765744c948a95fccbe46
Stephanie Marchante <smarcha04@gmail.com>
2019-04-09 16:08:49 -0700

    added root to in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # verb 'path', to: 'controller#action'
   get '/tasks', to: 'tasks#index'
-
   get '/tasks/:id', to: 'tasks#show'
+ 
+   root to: "tasks#index"
 end
