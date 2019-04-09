41a0082d896b219271a875a1aff9b827d90091db
Margaret Finnan <msfinnan@gmail.com>
2019-04-09 14:41:45 -0700

    Created a Task model and created tasks in rails console

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,6 @@
-TASKS = [
-  {task_name: "Task List Project", status: "in progress"},
-  {task_name: "CS Fun reading", status: "not started"},
-  {task_name: "CS Fun PR", status: "not started"},
-]
-
 class TasksController < ApplicationController
   def index
-    @tasks = TASKS
+    @tasks = Task.all
   end
+
 end
