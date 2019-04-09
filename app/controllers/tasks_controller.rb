b02540b1b6c3f724273c65430e6f24d46acef12d
Bitaman <b.amani.s@gmail.com>
2019-04-09 16:47:14 -0700

    generated the task model and run the migration

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,22 @@
+
+
+
 class TasksController < ApplicationController
+
     def index
+        @tasks = TASKS
+     
     end
+
+def show
+    task_id = params[:id]
+    puts "Task ID was #{task_id}"
+    puts "result of .to_i: #{task_id.to_i}"
+    @task = TASKS[task_id.to_i]
+
+    unless @task
+        head :not_found
+    end
+end
+
 end
