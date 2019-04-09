2a1c0a82982bee294431b241a73d728ee59bfa5e
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-04-09 15:55:26 -0700

    added Task model, migration to update completion_date column, added 2 Tasks to the database, and added the show action to the Task Controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,12 @@
 class TasksController < ApplicationController
-  TASKS = [
-    {name: "Practice CSS"},
-    {name: "Celebrate Wedding Anniversary"},
-    {name: "Stress Eat!"},
-  ]
-
   def index
-    @tasks = TASKS
+    @tasks = Task.all
+  end
+
+  def show
+    task_id = params[:id].to_i
+    @task = Task.find_by(id: task_id)
+
+    head :not_found if @task.nil?
   end
 end
