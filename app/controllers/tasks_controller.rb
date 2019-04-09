4b519a1a574fed095bc557b0db0f412a0b0c5abc
Karla Guadron <ksguadron@gmail.com>
2019-04-09 15:58:15 -0700

    Added instances of task and reworked index.html.erb as well as style sheet

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,5 @@
 class TasksController < ApplicationController
   def index
-    @tasks = [{ name: "watch movie", description: "Hidden Figures", due_date: "Wednesday"},
-      {name: "read book", description: "Kindred", due_date: "Friday"},
-      { name: "homework", description: "api project", due_date: "Monday"}]
+    @tasks = Task.all
   end
 end
