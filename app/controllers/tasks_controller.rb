366a0519e44c98156ae58120af4b635528c956fa
amyesh <amyesh08@gmail.com>
2019-04-09 15:34:12 -0700

    added Task model and updated index method

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,5 @@
 class TasksController < ApplicationController
-  TASKS = [
-    "Build TaskList Rails App",
-    "Move Joyfully",
-    "Go Grocery Shopping",
-  ]
-
   def index
-    @tasks = TASKS
+    @tasks = Task.all
   end
 end
