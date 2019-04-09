d4aa150588504c0484287b655d4791dea5d3330e
Pauline Sauget <paulinevsauget@gmail.com>
2019-04-09 16:04:39 -0700

    Show action, create database schema, create 2 Task model instances using rails console

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,20 @@
-TASKS = [ "Task One", "Task Two", "Task Three", "Task Four"
-]
-
-
 class TasksController < ApplicationController
+  TASKS = [ "Task One", "Task Two", "Task Three", "Task Four"
+  ]
+  
   def index
     @tasks = TASKS
+  end
+
+  def show
+    task_id = params[:id]
+
+    puts "Task ID was #{task_id}"
+
+    @task = TASKS[task_id.to_i]
+
+    unless @task
+      head :not_found
+    end
   end
 end
--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -1,7 +1,38 @@
 require "test_helper"
 
 describe TasksController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe "index" do
+    it "renders without crashing" do
+      # Arrange
+
+      # Act
+      get "/tasks"
+
+      # Assert
+      must_respond_with :ok
+    end
+
+    it "renders even when there is zero task" do
+      TasksController::TASKS.clear
+
+      get "/tasks"
+
+      must_respond_with :ok
+    end
+  end
+
+  describe "show" do
+    it "returns a 404 status code if the task doesn't exist" do
+      # Fail case
+      task_id = 12345
+
+      get "/tasks/#{task_id}"
+
+      must_respond_with :not_found
+    end
+
+    it "works for a task that exists" do
+
+    end
+  end
 end
