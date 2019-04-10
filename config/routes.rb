c1ab7305d0c138e3af72e7ab482e66006906d8b6
Stephanie Marchante <smarcha04@gmail.com>
2019-04-10 16:36:17 -0700

    added show link for tasks to click between tasks and descriptions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # verb 'path', to: 'controller#action'
-  get '/tasks', to: 'tasks#index'
+  get '/tasks', to: 'tasks#index', as: 'tasks'
 
   get '/tasks/:id', to: 'tasks#show'
  
