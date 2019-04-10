56d08e43bfdf2a569a8fec9d5529a16693f37cd7
Pauline Sauget <paulinevsauget@gmail.com>
2019-04-10 16:58:36 -0700

    Wrote methods for new & create

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,6 +4,24 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
+  def new
+    @task = Task.new
+  end
+
+  def create
+    puts "Made it!"
+
+    task = Task.new(
+      name: params["task"]["name"],
+      description: params["task"]["description"],
+      completion_date: params["task"]["completion_date"]
+    )
+
+    task.save
+
+    redirect_to tasks_path
+  end
+
   def show
     task_id = params[:id]
 
@@ -12,7 +30,8 @@ class TasksController < ApplicationController
     @task = Task.find_by(id: task_id)
 
     unless @task
-      head :not_found
+      # head :not_found
+      redirect_to tasks_path
     end
   end
 end
