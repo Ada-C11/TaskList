398cdeeb95140a76b215555436285105fa9eeaa3
Shubha Rajan <shubharajan94@gmail.com>
2019-04-10 14:27:40 -0700

    created views for show and new, wrote code for task#new and task#create

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,7 +7,20 @@ class TasksController < ApplicationController
   def show
     @task = Task.find_by(id: params[:id])
     if !@task
-      head :not_found
+      redirect_to tasks_path, :flash => { :error => "Could not find task with id: #{params[:id]}" }
+    end
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(name: params[:task][:name], description: params[:task][:description])
+    if @task.save
+      redirect_to task_path(@task.id), { :flash => { :success => "Successfully added task!" } }
+    else
+      redirect_to :new, :flash => { :error => "Failed to add task" }
     end
   end
 end
--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -36,7 +36,6 @@ describe TasksController do
     end
 
     it "will redirect for an invalid task" do
-      skip
       # Act
       get task_path(-1)
 
@@ -48,7 +47,6 @@ describe TasksController do
 
   describe "new" do
     it "can get the new task page" do
-      skip
 
       # Act
       get new_task_path
@@ -60,7 +58,6 @@ describe TasksController do
 
   describe "create" do
     it "can create a new task" do
-      skip
 
       # Arrange
       task_hash = {
@@ -78,8 +75,8 @@ describe TasksController do
 
       new_task = Task.find_by(name: task_hash[:task][:name])
       expect(new_task.description).must_equal task_hash[:task][:description]
-      expect(new_task.due_date.to_time.to_i).must_equal task_hash[:task][:due_date].to_i
-      expect(new_task.completed).must_equal task_hash[:task][:completed]
+
+      expect(new_task.completion_date).must_equal task_hash[:task][:completion_date]
 
       must_respond_with :redirect
       must_redirect_to task_path(new_task.id)
