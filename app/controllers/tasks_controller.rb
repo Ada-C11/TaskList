3f18d86f6e30319e34b33ad0433c28edf848e5dc
Margaret Finnan <msfinnan@gmail.com>
2019-04-10 16:22:26 -0700

    Added functionality to add a new task using a form

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,4 +11,25 @@ class TasksController < ApplicationController
     end
   end
 
+  def new
+    @task = Task.new 
+    @task.task_name = "Default task name"
+  end
+
+  def create
+    new_task = Task.new(
+      task_name: params["task"]["task_name"],
+      description: params["task"]["description"],
+      completion_date: params["task"]["completion_date"]   
+    )
+
+    created_successfully = new_task.save 
+
+    if created_successfully
+      redirect_to task_path(new_task.id)
+    else 
+      head :not_found
+    end
+  end 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
-  get '/tasks', to: 'tasks#index'
-  get '/tasks/:id', to: 'tasks#show'
+  get '/tasks', to: 'tasks#index', as: "tasks"
+
+  get "/tasks/new", to: "tasks#new", as: "new_task"
+  post "/tasks/", to: "tasks#create" #tasks 
+
+  get '/tasks/:id', to: 'tasks#show', as: "task"
+
+  get '/tasks/:id/edit', to: 'tasks#edit', as: "edit_task"
+  patch "/tasks/:id", to: "tasks#update" #path already has a name 
+  delete "/tasks/:id", to: "tasks#destroy" #path already has a name 
+
+  # mark a task as read / task as complete (not a RESTful route)
+  patch "tasks/:id/read", to: "tasks#mark_read", as: "mark_read"
+  
+  root to: "tasks#index" #makes the homepage for the site go to tasks#index 
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
