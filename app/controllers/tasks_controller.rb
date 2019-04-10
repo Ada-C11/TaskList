2434507b9765693a23886176e4b19900f1a3313a
Erica Norris <norrise120@gmail.com>
2019-04-10 14:54:30 -0700

    added tasks#create to pass tests (test was updated to remove due_date as this was not part of task model)

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,4 +16,15 @@ class TasksController < ApplicationController
   def new
     @task = Task.new
   end
+
+  def create
+    task = Task.new(
+      name: params["task"]["name"],
+      description: params["task"]["description"],
+    )
+
+    task.save
+
+    redirect_to task_path(task.id)
+  end
 end
