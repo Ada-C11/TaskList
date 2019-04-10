0e1683ac63e13d3f4c5744976a1f8c9aabaa47dd
Alexandria McCarthy <alexandria.mccarthy77@gmail.com>
2019-04-10 16:01:16 -0700

    fixed bugs in testing file

--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -1,9 +1,11 @@
 require "test_helper"
 
 describe TasksController do
+  # Note to students:  Your Task model **may** be different and
+  #   you may need to modify this.
   let (:task) {
     Task.create name: "sample task", description: "this is an example for a test",
-                completion_date: Time.now + 5.days
+                completed: false
   }
 
   # Tests for Wave 1
@@ -28,6 +30,7 @@ describe TasksController do
   # Unskip these tests for Wave 2
   describe "show" do
     it "can get a valid task" do
+      skip
       # Act
       get task_path(task.id)
 
@@ -42,7 +45,6 @@ describe TasksController do
 
       # Assert
       must_respond_with :redirect
-      expect(flash[:error]).must_equal "Could not find task with id: -1"
     end
   end
 
@@ -63,11 +65,13 @@ describe TasksController do
       skip
 
       # Arrange
+      # Note to students:  Your Task model **may** be different and
+      #   you may need to modify this.
       task_hash = {
         task: {
           name: "new task",
           description: "new task description",
-          completion_date: nil,
+          completed: false,
         },
       }
 
@@ -78,7 +82,6 @@ describe TasksController do
 
       new_task = Task.find_by(name: task_hash[:task][:name])
       expect(new_task.description).must_equal task_hash[:task][:description]
-      expect(new_task.due_date.to_time.to_i).must_equal task_hash[:task][:due_date].to_i
       expect(new_task.completed).must_equal task_hash[:task][:completed]
 
       must_respond_with :redirect
