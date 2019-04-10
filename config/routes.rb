6db8931e1435240c505f2185f6fdb41cac9512b3
Pauline Sauget <paulinevsauget@gmail.com>
2019-04-10 16:57:15 -0700

    Add route for new & create

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/tasks', to: 'tasks#index'
-  get '/tasks/:id', to: 'tasks#show'
+  root 'tasks#index'
+
+  # Routes that operate on the task collection
+  get "/tasks", to: "tasks#index", as: "tasks"
+  get "/tasks/new", to: "tasks#new", as: "new_task"
+  post "/tasks", to: "tasks#create"
+
+  # Routes that operate on individual tasks
+  get "/tasks/:id", to: "tasks#show", as: "task"
+
 end
