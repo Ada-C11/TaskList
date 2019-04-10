863429bbb624e88c321ae48eff9e35bc148ce7e8
Nara <naraa_520@yahoo.com>
2019-04-09 17:15:34 -0700

    Creates task model and db schema

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,12 @@
-TASKS = ['Finish the website', 'clean the house', 'sleep'].freeze
 class TasksController < ApplicationController
   def index
-    @tasks = TASKS
+    @tasks = Task.all
+  end
+
+  def show
+    task_id = params[:id]
+    @task = Task.find_by(id: task_id)
+
+    head :not_found if @task.nil?
   end
 end
