7140e61e60f4a9c6fe308c8321931b9bccb9bbae
sjscotton <sj.scotton@gmail.com>
2019-04-09 21:30:09 -0700

    added index

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,18 @@
 class TasksController < ApplicationController
+  # TASKS = [
+  #   { task: "Laundry", date: "04-5-18" },
+  #   { task: "Meal Prep", date: "04-5-19" },
+  # ]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    task_id = params[:id]
+
+    @task = Task.find(task_id)
+
+    head :not_found unless @task
+  end
 end
