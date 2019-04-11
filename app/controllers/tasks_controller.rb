6bbc51ce1f4c304666915e082205474e5c9fd1c8
ranuseh <rosalyn.anuseh@gmail.com>
2019-04-10 17:38:28 -0700

    Added new task with link to forms

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,20 +1,51 @@
-# require_relative '../models/task.rb'
-
-# TASKS = [
-#   { Task1: "Make Breakfast", description: "This is how I make breakfast", completion_date: "july 12 2019"},
-#   { Task2: "Go grocery shopping", description: "This is how I grocery shop", completion_date: "May 6 2019"},
-#   { Task3: "Walk the dog", description: "This is how I walk the dog", completion_date: "May 7 2019"},
-#   { Task4: "Finish website", description: "This is how I finish my website", completion_date: "June 10 2019"},
-#   { Task5: "Visit my brother", description: "This is how I visit my brother", completion_date: "April 19 2019"},
-#   { Task6: "Make birthday cards", description: "This is how I make birthday cards", completion_date: "May 1 2019"},
-#   { Task7: "Go to the gym", description: "This is how I go to the gym", completion_date: "June 17 2019"},
-# ]
-
 class TasksController < ApplicationController
-
-  def index # Loads a list of all the books
+  def index # Loads a list of all the tasks
     @tasks = Task.all
-    return @tasks
+
+    @urgent_task = @tasks.last
+
+  end
+
+  def show # shows the details of each task
+    task_id = params[:id]
+    puts "Task ID was #{task_id}"
+    puts "Result of .to_i: #{task_id.to_i}"
+
+    @task = Task.find_by(id: task_id)
+    unless @task
+      head :not_found
+    end
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(
+      name: params[:task][:name],
+      description: params[:task][:description],
+      completion_date: params[:task][:completion_date]
+      )
+
+    if @task.save # save returns true if the database insert succeeds
+      redirect_to root_path # go to the index so we can see the task in the list
+    else # save failed :(
+      render :new # show the new task form view again
+    end
+
+    unless params["task"]
+      render :new, status: :bad_request
+      return
+    end
+
+    @task.name = params["task"]["name"],
+    @task.description = params["task"]["description"]
+    @task.completion_date = params["task"]["completion_date"]
+
+    @task.save
+
+    redirect_to tasks_path
   end
 
 end
