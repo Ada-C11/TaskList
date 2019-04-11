e6e5493d938b583e7283908befacea1b4a41562a
K <774162+kaseea@users.noreply.github.com>
2019-04-11 09:59:02 -0700

    adds support to show all task, and adds ability to create new task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,27 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def show
+    task_id = params[:id].to_i
+    @task = Task.find_by(id: task_id)
+    if @task.nil?
+      redirect_to tasks_path
+    end
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(title: params[:task][:title], description: params[:task][:description], completion: params[:task][:completion]) #instantiate a new book
+    if @task.save # save returns true if the database insert succeeds
+      redirect_to task_path(@task.id)  #why not task_path
+      #   redirect_to root_path
+      # go to the index so we can see the book in the list
+    else # save failed :(
+      render :new # show the new book form view again
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
-  get "tasks/index"
+  # get "tasks/index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/tasks", to: "tasks#index"
+
+  #the fact that its tasks instead of task means I probably named everything wrong and maybe should fix
+  get "/tasks", to: "tasks#index", as: "tasks"
+
+  get "/tasks/new", to: "tasks#new", as: "new_task"
+  post "/tasks", to: "tasks#create"
+
+  get "/tasks/:id", to: "tasks#show", as: "task"
+
+  get "/tasks/:id/edit", to: "tasks#edit", as: "edit_task"
+  patch "tasks/:id", to: "tasks#update"
+
+  delete "/tasks/:id", to: "tasks#destroy"
+
+  root to: "tasks#index"
 end
