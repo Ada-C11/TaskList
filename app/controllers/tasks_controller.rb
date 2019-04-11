c343da483825ff7169b13bec298fe3c5d19d57e4
qqdipps <sdipp007@gmail.com>
2019-04-11 09:37:35 -0700

    all tests passing

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,7 +4,10 @@ class TasksController < ApplicationController
   end
 
   def show
-    @task = Task.find(params[:id])
+    task_id =  params[:id]
+    @task = Task.find_by(id: task_id)
+    flash[:error] = "Could not find task with id: #{task_id}"
+    redirect_to tasks_path unless @task
   end
 
   def new
