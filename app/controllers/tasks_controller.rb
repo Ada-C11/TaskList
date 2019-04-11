a0a36cf5fe9e291dd74883021d38e0a6bd2fd7d3
Stephanie Marchante <smarcha04@gmail.com>
2019-04-11 12:17:54 -0700

    added delete functionality, working on prompting the user if they were sure to delete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -57,4 +57,17 @@ class TasksController < ApplicationController
     end
   end
 
+  def destroy
+    task = Task.find_by(id:  params[:id] )
+
+    if task.nil?
+      head :not_found
+    else
+      task.destroy 
+      redirect_to tasks_path
+    end
+   
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   get "/tasks/:id/edit", to: "tasks#edit", as: "edit_task"
   patch "/tasks/:id", to: "tasks#update"
 
+  delete "/tasks/:id", to: "tasks#destroy"
+
  
   root to: "tasks#index"
 end
