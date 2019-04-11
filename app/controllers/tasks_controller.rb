699790a849bc1766423d696514f5134fcc785c4c
Stephanie Marchante <smarcha04@gmail.com>
2019-04-10 17:44:14 -0700

    successfully saved user inputted new task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -20,4 +20,25 @@ class TasksController < ApplicationController
     @task = Task.new(name: "Location")
   end
 
+  def create
+    task = Task.new(
+      name: params["task"]["name"],
+      completion_date: params["task"]["completion_date"],
+      description: params["task"]["description"]
+      )
+
+
+    # If the book saves correctly, then we want to redirect to the show page of that book
+    # Otherwise, we should give back something about the error (for now, 404)
+
+    is_successful = task.save
+
+    if is_successful
+      redirect_to task_path(task.id)
+    else
+      # For Rails Week 1, not a requirement (not possible right now?) to test this case in controller tests
+      head :not_found
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   get '/tasks', to: 'tasks#index', as: 'tasks'
 
   get '/tasks/new', to: 'tasks#new', as: 'new_task'
+  post "/tasks", to: "tasks#create" 
+
   get '/tasks/:id', to: 'tasks#show', as: 'task'
+
+  
  
   root to: "tasks#index"
 end
