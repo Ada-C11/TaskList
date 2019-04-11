226f53a531fe895cc28dc94f2172a814b9c7fe4e
Shamira <marshallscm@gmail.com>
2019-04-11 15:52:22 -0700

    Added new and create routes and methods to create a new task.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,7 +12,31 @@ class TasksController < ApplicationController
     task_id = params[:id].to_i
     @task = Task.find_by(id: task_id)
     if @task.nil?
-      head :not_found
+      head :temporary_redirect
+    end
+  end
+
+  def new
+    @task = Task.new(name: "Default")
+  end
+
+  def create
+    task = Task.new(
+      name: params["task"]["name"],
+      description: params["task"]["description"],
+      completion_date: params["task"]["completion_date"],
+    )
+
+    if task.name == "" || task.description == "" || task.completion_date == ""
+      task.save = false
+    end
+
+    is_successful = task.save
+
+    if is_successful
+      redirect_to task_path(task.id)
+    else
+      head :temporary_redirect
     end
   end
 end
