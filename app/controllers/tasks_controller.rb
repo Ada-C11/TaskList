43bb3dbca5211cc8cbf16c1e0397f275c90a7f2f
Karla Guadron <ksguadron@gmail.com>
2019-04-10 18:53:04 -0700

    Created new and show methods with accompanying view files and routes

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,35 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def show
+    task_id = params[:id]
+
+    @task = Task.find_by(id: task_id)
+
+    unless @task
+      flash[:error] = "Could not find task with id: -1"
+      redirect_to "tasks_path"
+    end
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new
+
+    @task.name = params["task"]["name"]
+    @task.description = params["task"]["description"]
+    @task.completion_date = params["task"]["completion_date"]
+
+    is_successful = @task.save
+
+    if is_successful
+      redirect_to task_path(@task.id)
+    else
+      head :not_found
+    end
+  end
 end
