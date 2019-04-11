4c6fc1aed9420f4adf20b89f2d7adcbdfcd3cfe4
Kirsten Anderson <kirsten@kirstenmunson.com>
2019-04-11 09:50:50 -0700

    edit action and view added

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -25,4 +25,18 @@ class TasksController < ApplicationController
 
     redirect_to tasks_path, flash: { alert: "Task added successfully" }
   end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    task = Task.find(params[:id])
+
+    # "task"=>{"name"=>"Feed bird again", "description"=>"Pellets and beans"}, "commit"=>"Save", "controller"=>"tasks", "action"=>"update", "id"=>"1"}
+
+    task.update(name: params["task"]["name"], description: params["task"]["description"], completion_date: params["task"]["completion_date"], completed: params["task"]["completed"])
+
+    redirect_to task_path(params[:id])
+  end
 end
