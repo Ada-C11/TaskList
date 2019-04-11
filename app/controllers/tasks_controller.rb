9daa627393fd028ac2e7fe77f7b24c7308e3e84c
amyesh <amyesh08@gmail.com>
2019-04-10 21:35:58 -0700

    added new_task and show pages

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,32 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def show
+    task_id = params[:id].to_i
+    @task = Task.find_by(id: task_id)
+    if @task.nil?
+      flash[:error] = "Could not find task with id: #{task_id}"
+      redirect_to task_path
+    end
+  end
+
+  def new
+    @task = Task.new(name: "name")
+  end
+
+  def create
+    task = Task.new(
+      name: params["task"]["name"],
+      description: params["task"]["description"],
+    )
+
+    is_successful = task.save
+
+    if is_successful
+      redirect_to task_path(task.id)
+    else
+      head :not_found
+    end
+  end
 end
--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -28,7 +28,6 @@ describe TasksController do
   # Unskip these tests for Wave 2
   describe "show" do
     it "can get a valid task" do
-      skip
       # Act
       get task_path(task.id)
 
@@ -37,7 +36,6 @@ describe TasksController do
     end
 
     it "will redirect for an invalid task" do
-      skip
       # Act
       get task_path(-1)
 
@@ -49,8 +47,6 @@ describe TasksController do
 
   describe "new" do
     it "can get the new task page" do
-      skip
-
       # Act
       get new_task_path
 
@@ -61,8 +57,6 @@ describe TasksController do
 
   describe "create" do
     it "can create a new task" do
-      skip
-
       # Arrange
       task_hash = {
         task: {
@@ -79,7 +73,7 @@ describe TasksController do
 
       new_task = Task.find_by(name: task_hash[:task][:name])
       expect(new_task.description).must_equal task_hash[:task][:description]
-      expect(new_task.due_date.to_time.to_i).must_equal task_hash[:task][:due_date].to_i
+      expect(new_task.completion_date).must_equal task_hash[:task][:completion_date]
       expect(new_task.completed).must_equal task_hash[:task][:completed]
 
       must_respond_with :redirect
