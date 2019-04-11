b4f6146b73bb4ffc860b422869bcc6352c50efad
qqdipps <sdipp007@gmail.com>
2019-04-11 08:33:40 -0700

    migration completed, added priorty column to tasks table'

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_10_035722) do
+ActiveRecord::Schema.define(version: 2019_04_11_153205) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2019_04_10_035722) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "priority_level"
+    t.integer "priority"
   end
 
 end
