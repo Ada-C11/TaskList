8939c8d2929ab7ee744b369b92d998db3d5f163a
Carla Bosco <carlabosco@gmail.com>
2019-04-11 17:54:05 -0700

    created edit and update methods

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,21 +1,69 @@
 class TasksController < ApplicationController
-  # TASKS = [
-  #   { one: "create a route to view the task index page" },
-  #   { two: "create a controller action for the task index page which contains an array of hard-coded tasks"},
-  #   { three: "create an ERB view to display the tasks from the controller action"}
-  # ]
-  
   def index
     @tasks = Task.all
   end
 
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new
+
+    unless params["task"]
+      render :new, status: :bad_request
+      return
+    end
+
+    @task.name = params["task"]["name"]
+    @task.description = params["task"]["description"]
+    @task.completion = params["task"]["completion"]
+
+    @task.save
+
+    redirect_to tasks_path
+  end
+
   def show
     task_id = params[:id]
-
     @task = Task.find_by(id: task_id) 
 
     unless @task
-      head :not_found #head is short for http headers
+      redirect_to tasks_path
+
     end
+  end
+
+  def destroy
+    task_id = params[:id]
+    task = Task.find_by(id: task_id)
+  
+    if task
+      task.destroy
+      redirect_to tasks_path
+    else
+      render :notfound, status: :not_found
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    
+    unless params["task"]
+      render :new, status: :bad_request
+      return
+    end
+
+    @task.name = params["task"]["name"]
+    @task.description = params["task"]["description"]
+    @task.completion = params["task"]["completion"]
+
+    @task.save
+
+    redirect_to tasks_path
   end
 end
