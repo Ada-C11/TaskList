38dc50e0ae5a8148e5cef34cf1e244b321a70fac
Sopheary Chiv <sophearychiv@gmail.com>
2019-04-12 14:27:52 -0700

    added a feature to create a new task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,4 @@
 class TasksController < ApplicationController
-  # TASKS = [
-  #   { task: "PR for CS Fun" },
-  #   { task: "TaskList project" },
-  # ]
-
   def index
     @tasks = Task.all
   end
@@ -13,7 +8,30 @@ class TasksController < ApplicationController
     @task = Task.find_by(id: task_id)
 
     if @task.nil?
-      head :not_found
+      redirect_to task_path
+      flash[:error] = "Could not find task with id: #{params[:id]}"
+    end
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    task = Task.new(
+      name: params["task"]["name"],
+      description: params["task"]["description"],
+      completion_date: params["task"]["completion_date"],
+      status: params["task"]["status"],
+    )
+
+    is_successful = task.save
+    if is_successful
+      redirect_to task_path(task.id)
+      flash[:success] = "Task successfully created!"
+    else
+      redirect_to task_path(task_id)
+      flash[:error] = "Cannot create task: #{params[:name]}"
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
-  get "/tasks", to: "tasks#index"
-  # get "/tasks/:id", to: "tasks#show"
-  get "/tasks/:id", to: "tasks#show", as: "task"
+  root to: "tasks#index"
+  # get "/tasks", to: "tasks#index", as: "tasks"
+
+  # # add a new task
+  # get "/tasks/new", to: "tasks#new", as: "new_task"
+  # post "/tasks", to: "tasks#create"
+
+  # get "/tasks/:id", to: "tasks#show", as: "task"
+
+  resources :tasks
+  # resources :tasks, only: [:index, :new, :create, :show]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root :to => "tasks#index"
+
 end
