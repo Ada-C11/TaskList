614f815da7ac3ac53dac56b332df3141287f7441
Kelly Downes <kedownes@gmail.com>
2019-04-12 10:54:46 -0700

    Add tests for edit and update

--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -88,13 +88,19 @@ describe TasksController do
   # Unskip and complete these tests for Wave 3
   describe "edit" do
     it "can get the edit page for an existing task" do
-      skip
-      # Your code here
+      # Act
+      get edit_task_path(task.id)
+
+      # Assert
+      must_respond_with :success
     end
 
     it "will respond with redirect when attempting to edit a nonexistant task" do
-      skip
-      # Your code here
+      # Act
+      get edit_task_path(-1)
+
+      # Assert
+      must_respond_with :redirect
     end
   end
 
@@ -103,13 +109,32 @@ describe TasksController do
     # Note:  If there was a way to fail to save the changes to a task, that would be a great
     #        thing to test.
     it "can update an existing task" do
-      skip
-      # Your code here
+      # Arrange
+      task = Task.create!(name: "original")
+      task_data = {
+        task: {
+          name: "changed",
+        },
+      }
+
+      # Act
+      patch task_path(task), params: task_data
+
+      # Assert
+      must_respond_with :redirect
+      must_redirect_to task_path(task)
+
+      task.reload
+      expect(task.title).must_equal(task_data[:book][:name])
     end
 
     it "will redirect to the root page if given an invalid id" do
-      skip
-      # Your code here
+      # Act
+      patch task_path(-1)
+
+      # Assert
+      must_respond_with :redirect
+      must_redirect_to root_path
     end
   end
 
