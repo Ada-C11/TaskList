d167f1fcbc11a8edd7f4eb3c2cfd3c9650300d0b
Mina Shin <minamishin@hotmail.com>
2019-04-12 15:21:42 -0700

    added form and option to create a new task

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,21 @@
+class TasksController < ApplicationController
+  # TASKS = [
+  #   { name: "Go to the store", day_due: "Monday" },
+  #   { name: "File taxes", day_due: "Wednesday" },
+  #   { name: "Buy cat food and litter", day_due: "Friday" },
+  #   { name: "Buy ferry ticket to Victoria", day_due: "Saturday" },
+  # ]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    task_id = params[:id].to_i
+    @task = Task.find_by(id: task_id)
+  end
+
+  def new
+    @task = Task.new
+  end
+end
