f5c8ae14adb61501d391fa19814f52d5586c523a
Jansen <v.jansen.martin@gmail.com>
2019-04-12 15:09:05 -0700

    Change default for 'completed' column to false

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_12_214351) do
+ActiveRecord::Schema.define(version: 2019_04_12_220110) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2019_04_12_214351) do
     t.string "completion_date", default: "f"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "completed"
+    t.boolean "completed", default: false
   end
 
 end
