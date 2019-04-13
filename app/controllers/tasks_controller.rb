41423ec47a76a0347c435f81d396d4ef92a1b1e9
Angela <angela.ohh@gmail.com>
2019-04-12 20:04:41 -0700

    fixed view for index when adding new tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -20,6 +20,7 @@ class TasksController < ApplicationController
 
   def create
     @task = Task.new(task_params) 
+    @task.update(completed: false)
     if @task.save 
       redirect_to task_path(@task.id)
     else
