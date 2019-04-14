46e266bbc89ba467ff8de69809ff60c079c583f3
Maria Wissler <wmcarmelina@gmail.com>
2019-04-13 22:45:30 -0700

    generate show

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,15 @@
+# frozen_string_literal: true
 
 class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def show
+    id = params[:id].to_i
+    @task = Task.find_by(id: id)
+
+    render :notfound, status: :not_found if @task.nil?
+  end
+
 end
