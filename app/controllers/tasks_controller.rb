df44e7e7feb354f2d8480563ee32f22c4209109d
Maria Wissler <wmcarmelina@gmail.com>
2019-04-14 00:18:00 -0700

    added show,new,create,edit and update with raw views

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,4 +12,39 @@ class TasksController < ApplicationController
     render :notfound, status: :not_found if @task.nil?
   end
 
+  def new
+    @task = Task.new(name: "-No Task Name-",
+                     description: "-No Description-",
+                     completion: "-No Completion Date-")
+  end
+
+  def create
+    @Task.new(
+      name: params[:task][:name],
+      description: params[:task][:description],
+      completion_date: params[:task][:completion],
+    )
+    if @task.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id].to_i)
+  end
+
+  def update
+    @task = Task.find_by(id: params[:id].to_i)
+    if @task.update(
+      name: params[:task][:name],
+      description: params[:task][:description],
+      completion_date: params[:task][:completion],
+    )
+      redirect_to task_path
+    else
+      render :find_by
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root 'tasks#index'
   get '/tasks', to: 'tasks#index', as: 'tasks'
   get '/tasks/new', to: 'tasks#new', as: 'new_task'
   post '/tasks', to: 'tasks#create'
