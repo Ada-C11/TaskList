f313d0f8b89a6e46ae26c04ea30b9f2f15665c68
Maria Wissler <wmcarmelina@gmail.com>
2019-04-13 21:30:51 -0700

    created data base, updated controller index to show all

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,6 @@
-# frozen_string_literal: true
-
-TASK_LIST = [
-  { id: 1, name: 'Wake up', description: 'only one alarm' },
-  { id: 2, name: 'Take a shower', description: 'limit to 15 min' }
-].freeze
 
 class TasksController < ApplicationController
   def index
-  @tasks = TASK_LIST
+    @tasks = Task.all
   end
 end
