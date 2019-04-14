4e88373e5f5a59be11964f943fdfb225b93095db
Maria Wissler <wmcarmelina@gmail.com>
2019-04-13 20:22:41 -0700

    hard coded task for baseline, shows in index

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,12 @@
+# frozen_string_literal: true
+
+TASK_LIST = [
+  { id: 1, name: 'Wake up', description: 'only one alarm' },
+  { id: 2, name: 'Take a shower', description: 'limit to 15 min' }
+].freeze
+
 class TasksController < ApplicationController
   def index
+  @tasks = TASK_LIST
   end
 end
