a4851c33c923e7402a876d319e571d6dfddfbdb7
Mina Shin <minamishin@hotmail.com>
2019-04-14 17:45:45 -0700

    wrote test for updating an existing task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,17 +1,10 @@
 class TasksController < ApplicationController
-  # TASKS = [
-  #   { name: "Go to the store", day_due: "Monday" },
-  #   { name: "File taxes", day_due: "Wednesday" },
-  #   { name: "Buy cat food and litter", day_due: "Friday" },
-  #   { name: "Buy ferry ticket to Victoria", day_due: "Saturday" },
-  # ]
-
   def index
     @tasks = Task.all
   end
 
   def show
-    task_id = params[:id].to_i
+    task_id = params[:id]
     @task = Task.find_by(id: task_id)
     if @task.nil?
       flash[:error] = "Could not find task with id: #{task_id}"
@@ -24,11 +17,47 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(name: params[:task][:name], description: params[:task][:description], completion_date: params[:task][:completion_date])
-    if @task.save
+    task = Task.new(name: params[:task][:name], description: params[:task][:description], completion_date: params[:task][:completion_date])
+    if task.save
       redirect_to root_path
     else
       render :new
     end
+  end
+
+  def edit
+    @task = Task.find_by(id: params[:id])
+    if @task.nil?
+      flash[:error] = "Could not find task with id: #{params[:id]}"
+      redirect_to action: "index", status: 302
+    end
+  end
+
+  def update
+    task = Task.find_by(id: params[:id])
+    if task.nil?
+      flash[:error] = "Could not find task with id: #{params[:id]}"
+      redirect_to action: "index", status: 302
+    else
+      task.update(task_params)
+      redirect_to task_path(task.id)
+    end
+  end
+
+  def destroy
+    task = Task.find_by(id: params[:id])
+    if task.nil?
+      flash[:error] = "Could not find task with id: #{params[:id]}"
+      redirect_to action: "index", status: 302
+    else
+      task.destroy
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def task_params
+    return params.require(:task).permit(:name, :description, :completed_date)
   end
 end
--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -92,13 +92,17 @@ describe TasksController do
   # Unskip and complete these tests for Wave 3
   describe "edit" do
     it "can get the edit page for an existing task" do
-      skip
-      # Your code here
+      # skip
+      get edit_task_path(task.id)
+
+      must_respond_with :success
     end
 
     it "will respond with redirect when attempting to edit a nonexistant task" do
-      skip
-      # Your code here
+      # skip
+      get edit_task_path(-1)
+
+      must_respond_with :redirect
     end
   end
 
@@ -107,20 +111,47 @@ describe TasksController do
     # Note:  If there was a way to fail to save the changes to a task, that would be a great
     #        thing to test.
     it "can update an existing task" do
-      skip
-      # Your code here
+      # skip
+      original_task = Task.create!(
+        name: "new task",
+        description: "new task description",
+        completion_date: "completion date",
+      )
+
+      new_hash = {
+        task: {
+          name: "new task name",
+        },
+      }
+
+      expect {
+        patch task_path(original_task.id), params: new_hash
+      }.wont_change "Task.count"
+
+      original_task.reload
+      expect(original_task.name).must_equal "new task name"
     end
 
     it "will redirect to the root page if given an invalid id" do
-      skip
-      # Your code here
+      # skip
+      patch task_path(-1)
+
+      must_respond_with :redirect
     end
   end
 
   # Complete these tests for Wave 4
   describe "destroy" do
-    # Your tests go here
+    it "can delete a task" do
+      new_task = Task.create(name: "start code hw")
 
+      expect {
+        delete task_path(new_task), params: new_task
+      }.must_change "Task.count", -1
+
+      must_respond_with :redirect
+      must_redirect_to task_path
+    end
   end
 
   # Complete for Wave 4
