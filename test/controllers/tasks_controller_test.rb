cb3f3a9f0444588568778d667ea01e07134e76c8
MyriamWD <myriam.waldend@gmail.com>
2019-04-14 17:42:48 -0700

    added tests fot edit action

--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -92,21 +92,13 @@ describe TasksController do
   # Unskip and complete these tests for Wave 3
   describe "edit" do
     it "can get the edit page for an existing task" do
-      skip
-
-      # it should update a task
-      # it should redirect to the show page of the task
-      # it should render 404 if the task was not found
-
-      # Your code here
+      get edit_task_path(task.id)
+      must_respond_with :success
     end
 
     it "will respond with redirect when attempting to edit a nonexistant task" do
-      skip
-      # Your code here
       invalid_id = "NOT A VALID ID"
-      # update task_path(invalid_id)
-      expect { edit task_path(invalid_id) }.wont_change "Task.count"
+      get edit_task_path(invalid_id)
       must_respond_with :redirect
     end
   end
