9e507cda1536f9deb182a1ac7f6e375ea161c37a
Amy Phung <phung.amy85@gmail.com>
2019-04-22 11:58:51 -0700

    revert changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_13_183653) do
+ActiveRecord::Schema.define(version: 2019_04_19_183843) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,19 @@ ActiveRecord::Schema.define(version: 2019_04_13_183653) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "completed_at"
+  end
+
+  create_table "tasks_categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tasks_categories_joins", force: :cascade do |t|
+    t.bigint "task_id"
+    t.bigint "category_id"
+    t.index ["category_id"], name: "index_tasks_categories_joins_on_category_id"
+    t.index ["task_id"], name: "index_tasks_categories_joins_on_task_id"
   end
 
 end
